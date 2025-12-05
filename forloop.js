0e8699c142b0169a61a5d7ca4c4b8cb145396abd
Msdmathan <89250756+Msdmathan@users.noreply.github.com>
2025-12-05 22:27:05 +0530

    Create forloop.js

---
+++ forloop.js
@@ -0,0 +1,14 @@
+// Sum of N numbers
+let n=5;
+let result=0;
+for(let i=1;i<=n;i++){
+  result+=i;
+}
+console.log(`The sum of first ${n} is ${result}`);
+
+// Factorial
+let res=1;
+for(let i=1;i<=n;i++){
+  res*=i;
+}
+console.log(`The ${n} Factorial is ${res}`);
